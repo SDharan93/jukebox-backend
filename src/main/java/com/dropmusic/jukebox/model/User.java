3c4e1816b0b3352f16dd3f9fc8472f85cd10ec0f
Shane Dharan <shanedharan@gmail.com>
2017-08-16 09:23:28 -0400

    Create User model

---
+++ src/main/java/com/dropmusic/jukebox/model/User.java
@@ -0,0 +1,63 @@
+package com.dropmusic.jukebox.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private Boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
