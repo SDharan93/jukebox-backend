68a8f1d0a2d132f6af626740422a9950040cb61a
Shane Dharan <shanedharan@gmail.com>
2017-08-16 09:25:21 -0400

    Modify DataConfig to use app.properties for database config

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- Insert users into database. Passwords are both 'password'
+insert into users (email,enabled,password) values ('shane@example.ca',true,'$2a$06$KQ7egivKNlSDLjMp7ODzQe/lYTjP71PZ7hiuSd9ffyf2MfA2KjHAi');
+insert into users (email,enabled,password) values ('shael@example.ca',true,'$2a$06$3PYE5JjJTsMwN/LKvJZgQu8czxdE0aKUZv0eTiGgvU4hOyOWlys1u');
