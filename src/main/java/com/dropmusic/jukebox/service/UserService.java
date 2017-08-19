4c402b1667ab574a1d6a8f3b01502e2eab9c7128
sdharan93 <shanedharan@gmail.com>
2017-08-19 16:59:04 -0400

    Create User Serice interface

---
+++ src/main/java/com/dropmusic/jukebox/service/UserService.java
@@ -0,0 +1,8 @@
+package com.dropmusic.jukebox.service;
+
+import com.dropmusic.jukebox.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    User findByEmail(String email);
+}
