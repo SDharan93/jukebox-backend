0a6f60c3b0704c08cbe8f5d33a67d4aa66ad589d
Shane Dharan <shanedharan@gmail.com>
2017-08-22 09:41:00 -0400

    Remove temp ignore url

---
+++ src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
@@ -0,0 +1,15 @@
+package com.dropmusic.jukebox.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+}
--- src/main/java/com/dropmusic/jukebox/config/SecurityConfig.java
+++ src/main/java/com/dropmusic/jukebox/config/SecurityConfig.java
@@ -38,8 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/health/**");
+
     }
 
     @Override
