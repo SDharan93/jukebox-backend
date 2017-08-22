8875e1838608a8e311e49a686b2738088b4ed8f6
Shane Dharan <shanedharan@gmail.com>
2017-08-22 09:51:06 -0400

    Create basic OAuth2 config

--- src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
+++ src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
@@ -1,8 +1,10 @@
 package com.dropmusic.jukebox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
@@ -12,4 +14,13 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Value("${oauth.tokenTimeout:3600}")
+    private int expiration;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("dropm").secret("secret").accessTokenValiditySeconds(expiration)
+                .scopes("read", "write").authorizedGrantTypes("password", "refresh_token").resourceIds("resource");
+    }
 }
