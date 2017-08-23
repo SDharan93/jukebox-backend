ed6cdd0acb77745079b298d039b7ba3cce7bffbe
Shane Dharan <shanedharan@gmail.com>
2017-08-23 10:27:25 -0400

    Add token tables

--- src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
+++ src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
@@ -2,13 +2,20 @@ package com.dropmusic.jukebox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,6 +27,22 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+
     @Value("${oauth.tokenTimeout:3600}")
     private int expiration;
 
@@ -27,6 +50,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
     public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
         configurer.authenticationManager(authenticationManager);
         configurer.userDetailsService(userDetailsService);
+        configurer.tokenStore(tokenStore());
     }
 
     @Override
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+select * from users;
+
+create table if not exists oauth_access_token (
+  token_id VARCHAR(256),
+  token BYTEA,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication BYTEA,
+  refresh_token VARCHAR(256)
+);
+
+create table if not exists oauth_refresh_token (
+  token_id VARCHAR(256),
+  token BYTEA,
+  authentication BYTEA
+);
