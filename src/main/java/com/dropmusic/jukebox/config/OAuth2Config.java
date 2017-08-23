7b370bdf4624ee9f066ab58a2fa84fa4e79551c8
Shane Dharan <shanedharan@gmail.com>
2017-08-22 22:46:00 -0400

    Fix OAuth token check

--- src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
+++ src/main/java/com/dropmusic/jukebox/config/OAuth2Config.java
@@ -3,10 +3,12 @@ package com.dropmusic.jukebox.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -15,8 +17,17 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter{
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Value("${oauth.tokenTimeout:3600}")
     private int expiration;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer configurer) throws Exception {
+        configurer.authenticationManager(authenticationManager);
+        configurer.userDetailsService(userDetailsService);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
