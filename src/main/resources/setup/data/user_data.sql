7b2920c8aaaaa125e397febbc78ee29279733cca
Shane Dharan <shanedharan@gmail.com>
2017-08-24 01:16:09 -0400

    Create tables using sql scripts instead of relying on spring

--- src/main/resources/setup/data/user_data.sql
+++ src/main/resources/setup/data/user_data.sql
@@ -1,3 +1,10 @@
 -- Insert users into database. Passwords are both 'password'
+INSERT INTO users (email,enabled,password)
+SELECT 'shane@example.ca',true,'$2a$06$KQ7egivKNlSDLjMp7ODzQe/lYTjP71PZ7hiuSd9ffyf2MfA2KjHAi'
+WHERE
+    NOT EXISTS (
+        SELECT email FROM users WHERE email = 'shane@example.ca'
+    );
+
 insert into users (email,enabled,password) values ('shane@example.ca',true,'$2a$06$KQ7egivKNlSDLjMp7ODzQe/lYTjP71PZ7hiuSd9ffyf2MfA2KjHAi');
 insert into users (email,enabled,password) values ('shael@example.ca',true,'$2a$06$3PYE5JjJTsMwN/LKvJZgQu8czxdE0aKUZv0eTiGgvU4hOyOWlys1u');
